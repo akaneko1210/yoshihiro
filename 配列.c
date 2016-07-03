fd31254da0f4b3ee5d97671a10f30879465eefe4
unknown <>
2016-07-04 03:48:10 +0900

    hairetsu

--- 配列.c
+++ 配列.c
@@ -3,9 +3,7 @@
 
 #include <stdio.h>
 int main (){
-	int a[10],i,sum;
-	int max=a[0];
-	int min=a[0];
+	int a[10],i,;
 
 
 	printf("input score\n");
@@ -13,18 +11,24 @@ int main (){
 	for(i=0;i<10;i++){
 		scanf("%d",&a[i]);
 	}
+	int max=a[0];
+	int min=a[0];
+
+	int sum=0;
+
+
 
 	for(i=0;i<10;i++){
 		sum+=a[i];
 
-	}
-
 	if (a[i]>max){
 		max=a[i];
-	}
+		}
 
-	if (a[i]<max){
+	if (a[i]<min){
 		min=a[i];
+		}
+
 	}
 
 	printf("sum:%4d\n",sum);
