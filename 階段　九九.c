07554c9bc8d6c128705bf36b9ce4ab4aa3ad9bcc
unknown <>
2016-07-04 02:07:59 +0900

    九九　三角

--- 階段　九九.c
+++ 階段　九九.c
@@ -6,6 +6,14 @@ int main (){
 
 	int i,j;
 
+
+	for(i=0;i<10;i++){
+		for(j=0;j<i+1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+
 	for(i=0;i<10;i++){
 		for(j=0;j<10-i;j++){
 			printf("*");
@@ -13,9 +21,13 @@ int main (){
 		printf("\n");
 	}
 
-	
 
-
+	for(i=1;i<10;i++){
+		for(j=1;j<10;j++){
+			printf("%3d",i*j);
+		}
+		printf("\n");
+	}
 
 
 	return 0;
